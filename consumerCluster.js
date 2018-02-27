7709b27aa1b51d590f28ef5a66d7ea9bda1bb149
nani <nani@nani-Vostro-15-3568>
2018-02-27 19:17:46 +0530

    listening on 2222, 3333 ports

--- consumerCluster.js
+++ consumerCluster.js
@@ -1,4 +1,5 @@
 var cluster = require('cluster');
+const http = require('http');
 var numCPUs = require('os').cpus().length;
 
 if (cluster.isMaster) {
@@ -17,4 +18,9 @@ if (cluster.isMaster) {
 } else {    
     var Consumer = require("./server/queue/consumer.js");
     new Consumer(Number(process.argv[2]));
+        
+    http.createServer((req, res) => {
+        res.writeHead(200);
+        res.end('I am alive\n');
+      }).listen(3333);
 }
