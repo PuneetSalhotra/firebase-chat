87a0908060ad5abed2117cef8508779e18432c60
nani <nani@nani-Vostro-15-3568>
2018-03-26 19:43:21 +0530

    Moved the following services to PamController /pam/asset/cover/alter/clockin /pam/asset/cover/alter/clockout

--- server/services/assetService.js
+++ server/services/assetService.js
@@ -1186,7 +1186,7 @@ function AssetService(objectCollection) {
     };
     
     //PAM
-    function assetListUpdateStatusPush(request, assetId){
+    /*function assetListUpdateStatusPush(request, assetId){
         return new Promise((resolve, reject)=>{
             var paramsArr = new Array(
                 assetId,
@@ -1213,7 +1213,7 @@ function AssetService(objectCollection) {
               });
             }
         });        
-    };
+    };*/
     
     var assetListUpdateLampStatus = function (request, assetId, callback) {
 
@@ -1463,7 +1463,7 @@ function AssetService(objectCollection) {
     };
 
     //PAM
-    this.assetClockIn = function (request, callback) {
+    /*this.assetClockIn = function (request, callback) {
         var dateTimeLog = util.getCurrentUTCTime();
         request['datetime_log'] = dateTimeLog;
         var response = {};
@@ -1493,7 +1493,22 @@ function AssetService(objectCollection) {
                         response.asset_id = resp.asset_id;
                         response.asset_message_counter = data;
                         response.asset_encryption_token_id = resp.asset_encryption_token_id;
-                        callback(false, response, 200);
+                        
+                        pamGetEmpStations(request).then((data)=>{
+                            if(data.length > 0) {                
+                                forEachAsync(data, function (next, row) {                    
+                                    pamAssetListUpdateOperatingAsset(request, row.asset_id, 0).then(()=>{
+                                        pamAssetListHistoryInsert(request, 40, row.asset_id).then(()=>{ 
+                                            next();
+                                            });
+                                        });                        
+                                })
+                            } 
+                          }).then(()=>{
+                              callback(false, response, 200);
+                          }).catch((err)=>{
+                              callback(true, err, -9999);
+                          });                        
                     } else {
                         callback(false, {}, -7998);
                     }
@@ -1562,7 +1577,7 @@ function AssetService(objectCollection) {
                 }
             });
         }
-    };
+    };*/
 
     //PAM
     this.assetStatsOnDutyTotal = function (request, callback) {
