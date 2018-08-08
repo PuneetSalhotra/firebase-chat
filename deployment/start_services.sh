bcd2a0740c59ce4d6fe6ed39eb61073ebb1b0595
nani <nani@nani-Vostro-15-3568>
2018-08-08 20:36:33 +0530

    Modified the file to handle both staging and production

--- deployment/start_services.sh
+++ deployment/start_services.sh
@@ -1,3 +1,12 @@
 #Pass arguments from command line
-cd /api-final-efs/node/staging/staging_desker_api
-mode=staging pm2 reload startStagProcesses.yml
+if [ "$DEPLOYMENT_GROUP_NAME" == "DeskerApiCode-Staging" ]
+then
+  cd /api-final-efs/node/staging/staging_desker_api
+  mode=staging pm2 reload startStagProcesses.yml
+elif [ "$DEPLOYMENT_GROUP_NAME" == "DeskerApiCode-Production" ] 
+then
+  cd /api-final-efs/node/production/desker_api
+  mode=prod pm2 reload startProdProcesses.yml
+else    
+  echo "Unknown deployment Group"
+fi
