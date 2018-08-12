d4fd7778ceeb8095b276adf3b77062edf8f03c84
Ben Sooraj <bensoorajm@gmail.com>
2018-08-12 17:06:45 +0530

    FormConfigController => /form/access/asset/timecard/list

--- server/controllers/formConfigController.js
+++ server/controllers/formConfigController.js
@@ -4,17 +4,18 @@
  */
 
 var FormConfigService = require("../services/formConfigService");
+const moment = require('moment');
 
 function FormConfigController(objCollection) {
-    
-    var responseWrapper = objCollection.responseWrapper;    
+
+    var responseWrapper = objCollection.responseWrapper;
     var app = objCollection.app;
 
     var formConfigService = new FormConfigService(objCollection);
 
     app.post('/' + global.config.version + '/form/access/organisation/list', function (req, res) {
-        
-        formConfigService.getOrganizationalLevelForms (req.body, function (err, data, statusCode) {
+
+        formConfigService.getOrganizationalLevelForms(req.body, function (err, data, statusCode) {
             if (err === false) {
                 // got positive response    
                 res.send(responseWrapper.getResponse(err, data, statusCode, req.body));
@@ -25,10 +26,10 @@ function FormConfigController(objCollection) {
             }
         });
     });
-    
+
     app.post('/' + global.config.version + '/form/access/account/list', function (req, res) {
-        
-        formConfigService.getAccountLevelForms (req.body, function (err, data, statusCode) {
+
+        formConfigService.getAccountLevelForms(req.body, function (err, data, statusCode) {
             if (err === false) {
                 // got positive response    
                 res.send(responseWrapper.getResponse(err, data, statusCode, req.body));
@@ -39,10 +40,10 @@ function FormConfigController(objCollection) {
             }
         });
     });
-    
+
     app.post('/' + global.config.version + '/form/access/workforce/list', function (req, res) {
-        
-        formConfigService.getWorkforceLevelForms (req.body, function (err, data, statusCode) {
+
+        formConfigService.getWorkforceLevelForms(req.body, function (err, data, statusCode) {
             if (err === false) {
                 // got positive response    
                 res.send(responseWrapper.getResponse(err, data, statusCode, req.body));
@@ -53,10 +54,10 @@ function FormConfigController(objCollection) {
             }
         });
     });
-    
+
     app.post('/' + global.config.version + '/form/access/activity/list', function (req, res) {
-        
-        formConfigService.getActivityLevelForms (req.body, function (err, data, statusCode) {
+
+        formConfigService.getActivityLevelForms(req.body, function (err, data, statusCode) {
             if (err === false) {
                 // got positive response    
                 res.send(responseWrapper.getResponse(err, data, statusCode, req.body));
@@ -67,10 +68,10 @@ function FormConfigController(objCollection) {
             }
         });
     });
-    
+
     app.post('/' + global.config.version + '/form/access/global/entry/collection', function (req, res) {
-        
-        formConfigService.getSpecifiedForm (req.body, function (err, data, statusCode) {
+
+        formConfigService.getSpecifiedForm(req.body, function (err, data, statusCode) {
             if (err === false) {
                 // got positive response    
                 res.send(responseWrapper.getResponse(err, data, statusCode, req.body));
@@ -81,7 +82,7 @@ function FormConfigController(objCollection) {
             }
         });
     });
-    
+
     //Added By Nani Kalyan for BETA
     app.post('/' + global.config.version + '/form/register/access/workforce/list', function (req, res) {
         formConfigService.getRegisterForms(req.body, function (err, data, statusCode) {
@@ -105,6 +106,7 @@ function FormConfigController(objCollection) {
         });
     });
 
+
     //Added By Nani Kalyan for BETA
     app.post('/' + global.config.version + '/form/register/access/workforce/list', function (req, res) {
         formConfigService.getRegisterForms(req.body, function (err, data, statusCode) {
@@ -116,9 +118,22 @@ function FormConfigController(objCollection) {
             }
         });
     });
-    
-    app.post('/' + global.config.version + '/form/access/workforce/timeline/list', function (req, res) {
-        formConfigService.getAllFormSubmissions(req.body, function (err, data, statusCode) {
+
+    // Endpoint to fetch specific Timecard Forms (all, 325, 800 or 801)
+    app.post('/' + global.config.version + '/form/access/asset/timecard/list', function (req, res) {
+        // Sanity check
+        if (req.body.datetime_start === undefined || req.body.datetime_end === undefined || req.body.flag === undefined) {
+            let data = 'One or more of the request parameters is missing.';
+            res.send(responseWrapper.getResponse(true, data, -3308, req.body));
+            return;
+        } else if (!moment(req.body.datetime_start, 'YYYY-MM-DD HH:mm:ss').isValid() || !moment(req.body.datetime_end, 'YYYY-MM-DD HH:mm:ss').isValid()) {
+            let data = 'Invalid datetime format.';
+            res.send(responseWrapper.getResponse(true, data, -3308, req.body));
+            return;
+        }
+
+        // Fetch the Timecard Forms
+        formConfigService.getManualMobileAndWebTimecardForms(req.body, function (err, data, statusCode) {
             if (err === false) {
                 res.send(responseWrapper.getResponse(err, data, statusCode, req.body));
             } else {
