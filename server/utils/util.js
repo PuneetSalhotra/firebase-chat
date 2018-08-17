8a409d4be1a614275a1c9b2fffe6ef4ab0bd8362
Ben Sooraj <bensoorajm@gmail.com>
2018-08-17 19:05:36 +0530

    getStartDateTimeOfWeek

--- server/utils/util.js
+++ server/utils/util.js
@@ -408,6 +408,11 @@ function Util() {
         var value = moment().startOf('week').add(1, 'days').format("YYYY-MM-DD");
         return value;
     };
+
+    this.getStartDateTimeOfWeek = function () {
+        var value = moment().startOf('week').add(1, 'days').format("YYYY-MM-DD HH:mm:ss");
+        return value;
+    };
     
     this.getEndDayOfWeek = function () {
         var value = moment().endOf('week').format("YYYY-MM-DD");
