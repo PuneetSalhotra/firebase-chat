7add611f685f4a302b8b6ba1bf9a43e58902afc5
nani <nani@nani-Vostro-15-3568>
2018-08-27 16:35:23 +0530

    Deployment 02 for the day - CD/CI - Storing in a non-efs place

--- deployment/start_services.sh
+++ deployment/start_services.sh
@@ -1,11 +1,11 @@
 #Pass arguments from command line
 if [ "$DEPLOYMENT_GROUP_NAME" == "DeskerApiCode-Staging" ]
 then
-  cd /api-staging-efs/node/staging_desker_api/2018-08-27-01/
+  cd /api-staging-efs/node/staging_desker_api/2018-08-27-02/
   mode=staging pm2 reload startStagProcesses.yml
 elif [ "$DEPLOYMENT_GROUP_NAME" == "DeskerApiCode-Production" ] 
 then
-  cd /api-itsnotan-efs/node/production_desker_api/2018-08-27-01/
+  cd /api-itsnotan-efs/node/production_desker_api/2018-08-27-02/
   mode=prod pm2 reload startProdProcesses.yml
 else    
   echo "Unknown deployment Group"
