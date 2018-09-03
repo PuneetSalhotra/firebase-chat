5eefb94e822c4b14865fbc29d0905cb5f135a5ed
nani <nani@nani-Vostro-15-3568>
2018-09-03 22:37:45 +0530

    Code Deploy 05 - 3rd Sep

--- deployment/start_services.sh
+++ deployment/start_services.sh
@@ -5,10 +5,11 @@ then
   mode=staging pm2 reload startStagProcesses.yml
 elif [ "$DEPLOYMENT_GROUP_NAME" == "DeskerApiCode-Production" ] 
 then
-  cd /api-itsnotan-efs/node/production_desker_api/2018-09-03-04/
+  cd /api-itsnotan-efs/node/production_desker_api/2018-09-03-05/
   mode=prod pm2 reload startProdProcesses.yml
-  #cd /api-final-efs/node/production_portal_api/2018-08-29-01
-  #mode=prod pm2 reload startAllProdPortalProcesses.yml  
+  sleep 2
+  cd /api-final-efs/node/production_portal_api/2018-08-29-01
+  mode=prod pm2 reload startAllProdPortalProcesses.yml
 else    
   echo "Unknown deployment Group"
 fi
