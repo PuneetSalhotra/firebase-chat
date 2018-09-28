f3a26662cc7d93a673bcc0736675dca84272f80a
Ben Sooraj <bensoorajm@gmail.com>
2018-09-28 15:58:18 +0530

    Suzuki Dealer Proforma Invoice Logic

--- server/services/activityCommonService.js
+++ server/services/activityCommonService.js
@@ -2062,6 +2062,47 @@ function ActivityCommonService(db, util, forEachAsync) {
         }
     };
 
+    // Update activity_master_data. This is being used for storing the pdfMake's 
+    // document definition in a JSON Format for Suzuki
+    this.updateActivityMasterData = function (request, activityId, activityMasterData, callback) {
+        // IN p_activity_id BIGINT(20), IN p_organization_id BIGINT(20), 
+        // IN p_activity_master_data JSON
+        var paramsArr = new Array(
+            activityId,
+            request.organization_id,
+            activityMasterData
+        );
+        var queryString = util.getQueryString('ds_p1_activity_list_update_master_data', paramsArr);
+        if (queryString !== '') {
+            db.executeQuery(0, queryString, request, function (err, data) {
+                if (err === false) {
+                    callback(false, data);
+                } else {
+                    callback(err, false);
+                }
+            });
+        }
+    };
+    
+    // Fetch contact file's first collaborator (non-creator):
+    this.fetchContactFileFirstCollaborator = function (request, activityId, callback) {
+        // IN p_activity_id BIGINT(20), IN p_organization_id BIGINT(20), 
+        // IN p_start_from SMALLINT(6), IN p_limit_value TINYINT(4)
+
+        var paramsArr = new Array(
+            activityId,
+            request.organization_id,
+            0,
+            1
+        );
+        var queryString = util.getQueryString('ds_p1_activity_asset_mapping_select_non_creator_participants', paramsArr);
+        if (queryString !== '') {
+            db.executeQuery(1, queryString, request, function (err, data) {
+                (err === false) ? callback(false, data): callback(true, {});
+            });
+        }
+    };
+
 };
 
 
