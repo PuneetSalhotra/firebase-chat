1ab785690f84c42f2eca0dd5bd82203762d2b568
nani <nani@nani-Vostro-15-3568>
2018-10-15 23:12:00 +0530

    modified the start_services file

--- deployment/start_services.sh
+++ deployment/start_services.sh
@@ -2,7 +2,7 @@
 if [ "$DEPLOYMENT_GROUP_NAME" == "DeskerApiCode-Staging" ]
 then
   cd /api-staging-efs/node/staging_desker_api/
-  #mode=staging pm2 reload startStagProcesses.yml
+  mode=staging pm2 reload startStagProcesses.yml
 else    
   echo "Unknown deployment Group"
 fi
