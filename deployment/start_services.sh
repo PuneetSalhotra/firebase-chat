1b7f8820155dabd4046f8a3774fe9c35f5aa3092
nani <nani@nani-Vostro-15-3568>
2018-11-26 13:37:07 +0530

    Release on 26th Nov

--- deployment/start_services.sh
+++ deployment/start_services.sh
@@ -5,7 +5,7 @@ then
   mode=preprod pm2 reload startPreProdProcesses.yml
 elif [ "$DEPLOYMENT_GROUP_NAME" == "prodCodeDeploy-Group" ] 
 then
-  cd /production_desker_api/2018-11-25-01/
+  cd /production_desker_api/2018-11-26-01/
   mode=prod pm2 reload startProdProcesses.yml
   #sleep 2
   #cd /api-final-efs/node/production_portal_api
