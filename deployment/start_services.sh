0182a28d21a066bc17c956bc54f63655e9e9377e
Ben Sooraj <bensoorajm@gmail.com>
2018-11-28 08:14:49 +0530

    Release #1 for the day

--- deployment/start_services.sh
+++ deployment/start_services.sh
@@ -5,7 +5,7 @@ then
   mode=preprod pm2 start startPreProdProcesses.yml
 elif [ "$DEPLOYMENT_GROUP_NAME" == "prodCodeDeploy-Group" ] 
 then
-  cd /production_desker_api/2018-11-26-04/
+  cd /production_desker_api/2018-11-28-01/
   mode=prod pm2 reload startProdProcesses.yml
   #sleep 2
   #cd /api-final-efs/node/production_portal_api
