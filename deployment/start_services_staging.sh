ecc77fe9f3d66bbba199f329995ce8e16d6328a5
nani <nani@nani-Vostro-15-3568>
2018-12-10 10:47:31 +0530

    New Staging Release on 10th Dec 2018 01

--- deployment/start_services_staging.sh
+++ deployment/start_services_staging.sh
@@ -4,7 +4,7 @@ then
   cd /staging_desker_api/
   pm2 delete /^Staging_Consumer/
   pm2 delete /^Staging_Main/
-  mode=staging pm2 reload startStagProcesses.yml
+  pm2 start stagingProcesses.yml
 else    
   echo "Unknown deployment Group"
 fi
