b0edbaee2c965d52d865fb238cc972a0d9ae782e
nani <nani@nani-Vostro-15-3568>
2018-12-17 18:12:08 +0530

    Staging Release on 17Dec

--- deployment/start_services_staging.sh
+++ deployment/start_services_staging.sh
@@ -1,9 +1,11 @@
 #Pass arguments from command line
 if [ "$DEPLOYMENT_GROUP_NAME" == "codeDeployOnStagingGroup" ]
 then
-  cd /staging_desker_api/
-  pm2 delete /^Staging_Consumer/
+  cd /staging_desker_api/  
   pm2 delete /^Staging_Main/
+  pm2 delete /^Staging_Consumer/
+  pm2 delete /^Staging_Widget/
+  pm2 delete /^Staging_Log/
   pm2 start stagingProcesses.yml
 else    
   echo "Unknown deployment Group"
