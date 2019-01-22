ccd0d27e01e2757e379362c17bbb88a85608d080
Ben Sooraj <bensoorajm@gmail.com>
2019-01-22 17:22:18 +0530

    code indentation

--- server/controllers/formConfigController.js
+++ server/controllers/formConfigController.js
@@ -150,12 +150,12 @@ function FormConfigController(objCollection) {
             }
         });
     });
-    
-    
+
+
     //Get the list of forms submitted by the user
     //Search the list of forms submitted by the user
     app.post('/' + global.config.version + '/form/access/user/list', function (req, res) {
-        formConfigService.getSearchUserForms(req.body, function (err, data, statusCode) {        
+        formConfigService.getSearchUserForms(req.body, function (err, data, statusCode) {
             if (err === false) {
                 res.send(responseWrapper.getResponse(err, data, statusCode, req.body));
             } else {
@@ -164,8 +164,8 @@ function FormConfigController(objCollection) {
             }
         });
     });
-    
-    
+
+
     app.put('/' + global.config.version + '/form/activity/alter', function (req, res) {
         var deviceOsId = 0;
         if (req.body.hasOwnProperty('device_os_id'))
@@ -179,30 +179,30 @@ function FormConfigController(objCollection) {
                 payload: req.body
             };
 
-            queueWrapper.raiseActivityEvent(event, req.body.activity_id, (err, resp)=>{
-                        if(err) {
-                            //console.log('Error in queueWrapper raiseActivityEvent : ' + resp)
-                            //global.logger.write('serverError',"Error in queueWrapper raiseActivityEvent",err,req);
-                            res.send(responseWrapper.getResponse(false, {}, -5999,req.body));
-                            throw new Error('Crashing the Server to get notified from the kafka broker cluster about the new Leader');
-                        } else {
-                            if (req.hasOwnProperty('device_os_id')) {
-                                if (Number(req.device_os_id) !== 5) {
-                                    //incr the asset_message_counter                        
-                                    cacheWrapper.setAssetParity(req.asset_id, req.asset_message_counter, function (err, status) {
-                                        if (err) {
-                                            //console.log("error in setting in asset parity");
-                                            global.logger.write('serverError', "error in setting in asset parity", err, req.body);
-                                        } else
-                                            //console.log("asset parity is set successfully")
-                                            global.logger.write('debug', "asset parity is set successfully", {}, req.body);
+            queueWrapper.raiseActivityEvent(event, req.body.activity_id, (err, resp) => {
+                if (err) {
+                    //console.log('Error in queueWrapper raiseActivityEvent : ' + resp)
+                    //global.logger.write('serverError',"Error in queueWrapper raiseActivityEvent",err,req);
+                    res.send(responseWrapper.getResponse(false, {}, -5999, req.body));
+                    throw new Error('Crashing the Server to get notified from the kafka broker cluster about the new Leader');
+                } else {
+                    if (req.hasOwnProperty('device_os_id')) {
+                        if (Number(req.device_os_id) !== 5) {
+                            //incr the asset_message_counter                        
+                            cacheWrapper.setAssetParity(req.asset_id, req.asset_message_counter, function (err, status) {
+                                if (err) {
+                                    //console.log("error in setting in asset parity");
+                                    global.logger.write('serverError', "error in setting in asset parity", err, req.body);
+                                } else
+                                    //console.log("asset parity is set successfully")
+                                    global.logger.write('debug', "asset parity is set successfully", {}, req.body);
 
-                                    });
-                                }
-                            }
-                            res.send(responseWrapper.getResponse(false, {}, 200,req.body));
+                            });
                         }
-                });
+                    }
+                    res.send(responseWrapper.getResponse(false, {}, 200, req.body));
+                }
+            });
             //res.send(responseWrapper.getResponse(false, {}, 200,req.body));
             //return;
         };
@@ -212,19 +212,19 @@ function FormConfigController(objCollection) {
             global.logger.write('debug', "json is fine", {}, req.body);
 
         } catch (exeption) {
-            res.send(responseWrapper.getResponse(false, {}, -3308,req.body));
+            res.send(responseWrapper.getResponse(false, {}, -3308, req.body));
             return;
         }
         if (util.hasValidActivityId(req.body)) {
             if ((util.isValidAssetMessageCounter(req.body)) && deviceOsId !== 5) {
                 cacheWrapper.checkAssetParity(req.body.asset_id, Number(req.body.asset_message_counter), function (err, status) {
                     if (err) {
-                        res.send(responseWrapper.getResponse(false, {}, -7998,req.body));
+                        res.send(responseWrapper.getResponse(false, {}, -7998, req.body));
                     } else {
-                        if (status) {     // proceed
+                        if (status) { // proceed
                             proceedInlineUpdate();
-                        } else {  // this is a duplicate hit,
-                            res.send(responseWrapper.getResponse(false, {}, 200,req.body));
+                        } else { // this is a duplicate hit,
+                            res.send(responseWrapper.getResponse(false, {}, 200, req.body));
                         }
                     }
                 });
@@ -233,19 +233,19 @@ function FormConfigController(objCollection) {
                 proceedInlineUpdate();
 
             } else {
-                res.send(responseWrapper.getResponse(false, {}, -3304,req.body));
+                res.send(responseWrapper.getResponse(false, {}, -3304, req.body));
             }
         } else {
-            res.send(responseWrapper.getResponse(false, {}, -3301,req.body));
+            res.send(responseWrapper.getResponse(false, {}, -3301, req.body));
         }
 
     });
-    
+
     app.post('/' + global.config.version + '/form/field/combo/list', function (req, res) {
-        formConfigService.getFormFieldComboValues(req.body).then((data)=>{   
+        formConfigService.getFormFieldComboValues(req.body).then((data) => {
             //console.log(data);
             res.send(responseWrapper.getResponse({}, data, 200, req.body));
-        }).catch((err) => { 
+        }).catch((err) => {
             let data = {};
             res.send(responseWrapper.getResponse(err, data, -999, req.body));
         });
@@ -392,7 +392,7 @@ function FormConfigController(objCollection) {
 
     // });
 
-    
+
     app.post('/' + global.config.version + '/form/transaction/data', function (req, res) {
 
         formConfigService.getFormTransactionData(req.body).then((data) => {
@@ -476,7 +476,7 @@ function FormConfigController(objCollection) {
             res.send(responseWrapper.getResponse(err, widgetListData, -9999, req.body));
         }
     });
-    
+
     app.post('/' + global.config.version + '/data_type/list', function (req, res) {
 
         formConfigService.getDataTypeList(req.body).then((data) => {
