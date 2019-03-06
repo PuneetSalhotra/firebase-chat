d21a006e993cf60486103a37afb391b13c657362
nani <nani@nani-Vostro-15-3568>
2019-03-06 23:42:21 +0530

    stopped the consumerZero on codedeployment

--- deployment/start_services_preprod_prod.sh
+++ deployment/start_services_preprod_prod.sh
@@ -7,6 +7,7 @@ elif [ "$DEPLOYMENT_GROUP_NAME" == "prodCodeDeploy-Group" ]
 then
   cd /production_desker_api/2019-02-27-01/
   pm2 start prodProcesses.yml
+  pm2 stop ConsumerZero
   sleep 2
   cd /production_portal_api/portal/ 
   npm install 
