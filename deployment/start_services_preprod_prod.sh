147442007dcd781910dbce45e2403351cdbb78a6
nani <nani@nani-Vostro-15-3568>
2019-03-08 11:39:07 +0530

    single partition for logs & Widgets in production

--- deployment/start_services_preprod_prod.sh
+++ deployment/start_services_preprod_prod.sh
@@ -8,6 +8,8 @@ then
   cd /production_desker_api/2019-02-27-01/
   pm2 start prodProcesses.yml
   pm2 stop ConsumerZero
+  pm2 stop Widget_ConsumerZero
+  pm2 stop Log_ConsumerZero
   sleep 2
   cd /production_portal_api/portal/ 
   npm install 
