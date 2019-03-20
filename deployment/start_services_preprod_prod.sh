86b5bc757f9334836e9514f7ac86c2c31af2e744
nani <nani@nani-Vostro-15-3568>
2019-03-20 18:21:16 +0530

    Preprod Release

--- deployment/start_services_preprod_prod.sh
+++ deployment/start_services_preprod_prod.sh
@@ -6,7 +6,11 @@ then
 elif [ "$DEPLOYMENT_GROUP_NAME" == "tempdeploy-Group" ] 
 then
   cd /preprod_desker_api/
-  pm2 reload preProdProcesses.yml  
+  pm2 delete PreProd_Main
+  pm2 delete PreProd_ConsumerZero
+  pm2 delete PreProd_Log_ConsumerZero
+  pm2 delete PreProd_Widget_ConsumerZero
+  pm2 start preProdProcesses.yml  
 elif [ "$DEPLOYMENT_GROUP_NAME" == "prodCodeDeploy-Group" ] 
 then
   cd /production_desker_api/2018-11-28-02/
