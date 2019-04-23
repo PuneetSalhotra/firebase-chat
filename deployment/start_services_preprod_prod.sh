5baa2cb0718a4f205bab7945c396d1ca7addf102
nani <nani@nani-Vostro-15-3568>
2019-04-23 10:15:43 +0530

    Production Release on 23rd April

--- deployment/start_services_preprod_prod.sh
+++ deployment/start_services_preprod_prod.sh
@@ -5,7 +5,7 @@ then
   pm2 reload preProdProcesses.yml
 elif [ "$DEPLOYMENT_GROUP_NAME" == "prodCodeDeploy-Group" ] 
 then
-  cd /production_desker_api/2019-04-18-01/
+  cd /production_desker_api/2019-04-23-01/
   pm2 start prodProcesses.yml
   pm2 stop ConsumerZero
   pm2 stop Widget_ConsumerZero
