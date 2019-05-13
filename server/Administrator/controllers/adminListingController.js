22588fca3216b5fbf169ac7ceff91f49ca94690d
Ben Sooraj <bensoorajm@gmail.com>
2019-05-13 13:22:35 +0530

    activityAssetMappingSelectCategoryContacts and getUnreadActivityCount porting

--- server/Administrator/controllers/adminListingController.js
+++ server/Administrator/controllers/adminListingController.js
@@ -42,6 +42,30 @@ function AdminListingController(objCollection) {
         }
     });
 
+    // Portal: activityAssetMappingSelectCategoryContacts
+    // DB Call: ds_p1_activity_asset_mapping_select_category_contacts
+    app.post('/' + global.config.version + '/admin/organization/coworker_contact/list', async function (req, res) {
+        const [err, orgData] = await adminListingService.activityAssetMappingSelectCategoryContacts(req.body);
+        if (!err) {
+            res.send(responseWrapper.getResponse({}, orgData, 200, req.body));
+        } else {
+            console.log("/admin/organization/coworker_contact/list | Error: ", err);
+            res.send(responseWrapper.getResponse(err, orgData, -9999, req.body));
+        }
+    });
+
+    // Portal: getUnreadActivityCount
+    // DB Call: ds_p1_activity_asset_mapping_select_unrd_cnt_team_floor
+    app.post('/' + global.config.version + '/admin/workforce/unread/count', async function (req, res) {
+        const [err, orgData] = await adminListingService.workforceDesksUnreadCount(req.body);
+        if (!err) {
+            res.send(responseWrapper.getResponse({}, orgData, 200, req.body));
+        } else {
+            console.log("/admin/workforce/unread/count | Error: ", err);
+            res.send(responseWrapper.getResponse(err, orgData, -9999, req.body));
+        }
+    });
+
 };
 
 module.exports = AdminListingController;
