23ab75bde20d9ca7f85033cb5e6a101f1eb9549e
sravan7777 <sravan@blueflock.com>
2019-05-30 19:35:10 +0530

    invalid caf annexure message change

--- server/services/assetService.js
+++ server/services/assetService.js
@@ -4066,11 +4066,11 @@ function AssetService(objectCollection) {
                         console.log(col +""+row +" : "+ workbook.Sheets[sheet_name_list[0]][`${col}${row}`].v);
                     }else{
                         console.log("Not a Number at "+ col +""+row +" : "+ workbook.Sheets[sheet_name_list[0]][`${col}${row}`].v);
-                        return ["error", "Not a Number at "+ col +""+row +" :: value : "+ workbook.Sheets[sheet_name_list[0]][`${col}${row}`].v];
+                        return ["error", "Please upload the predefined format for CAF Annexure"];
                     }
                     
                 } catch (error) {
-                    return [error,""];
+                    return [error, "Please upload the predefined format for CAF Annexure."];
                 }
             }
         }
