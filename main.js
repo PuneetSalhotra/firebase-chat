b22f8277fdeb93327e26f4996fe32f46445f8913
Ben Sooraj <bensoorajm@gmail.com>
2019-08-13 09:23:38 +0530

    a little more fine tuning morgan

--- main.js
+++ main.js
@@ -120,7 +120,8 @@ app.use(function (req, res, next) {
 const logger = require('./server/logger/winstonLogger');
 loggerstream = {
     write: function (message, encoding) {
-        logger.info(message, {type: 'http_log'});
+        message = JSON.parse(message);
+        logger.info(`${message.method} ${message.url}`, { type: 'http_log', ...message });
     }
 };
 app.use(require("morgan")('{"remote_addr": ":remote-addr", "remote_user": ":remote-user", "date": ":date[clf]", "method": ":method", "url": ":url", "http_version": ":http-version", "status": ":status", "result_length": ":res[content-length]", "referrer": ":referrer", "user_agent": ":user-agent", "response_time": ":response-time"}', { stream: loggerstream }));
