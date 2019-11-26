0e1488987dc055c842c90c19aa2747eed5e708c2
nani <nani@nani-Vostro-15-3568>
2019-11-26 23:35:18 +0530

    modified start_services_srpint.sh

--- deployment/start_services_sprint.sh
+++ deployment/start_services_sprint.sh
@@ -2,9 +2,9 @@
 if [ "$DEPLOYMENT_GROUP_NAME" == "sprintServicesCodeDeploy-dg" ]
 then
   cd /sprint_desker_api/
-  pm2 delete /^sprint_Consumer/
-  pm2 delete /^sprint_Main/
-  mode=sprint pm2 reload sprintProcesses.yml
+  pm2 delete /^Sprint_ConsumerZero/
+  pm2 delete /^Sprint_Main/
+  pm2 reload sprintProcesses.yml
 else    
   echo "Unknown deployment Group"
 fi
