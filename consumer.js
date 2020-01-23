799655a473d81333e0ff89eefe6656848ce0dd85
Ben Sooraj <bensoorajm@gmail.com>
2020-01-23 09:13:17 +0530

    log out process errors to datadog

--- consumer.js
+++ consumer.js
@@ -1,14 +1,22 @@
 var Consumer = require("./server/queue/consumer.js");
 new Consumer();
 
-process.on('uncaughtException', (err) => {
-  console.log(`process.on(uncaughtException): ${err}\n`);
-  //throw new Error('uncaughtException');
+const logger = require('./server/logger/winstonLogger');
+
+process.on('uncaughtException', (error, origin) => {
+    logger.error("Uncaught Exception", { type: 'uncaught_exception', origin, error: error });
+    // console.log(`process.on(uncaughtException): ${err}\n`);
+    // throw new Error('uncaughtException');
 });
 
-process.on('error', (err) => {
-  console.log(`process.on(error): ${err}\n`);
-  throw new Error('error');
+process.on('error', (error) => {
+    // console.log(`process.on(error): ${err}\n`);
+    logger.error("Process Error", { type: 'process_error', error: error });
+    throw new Error('error');
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    logger.error("Unhandled Promise Rejection", { type: 'unhandled_rejection', promise_at: promise, error: reason });
 });
 
 /*var http = require('http');
