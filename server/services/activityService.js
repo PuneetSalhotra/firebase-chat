b8ca87ebc21aa4d09f06ed94e0401a1f1c9b94d7
nani <nani@nani-Vostro-15-3568>
2020-03-23 20:08:09 +0530

    Fixed issue in - /admin/workforce/desk/add Fixed issue in - Expense form submission value issue

--- server/services/activityService.js
+++ server/services/activityService.js
@@ -399,14 +399,22 @@ function ActivityService(objectCollection) {
                                                     console.log('LOOP ELSE ::' + request.activity_type_id + ' ' + fieldObj.field_id);
                                                     if(Object.keys(creditDebitFields).includes(String(fieldObj.field_id))){
                                                         let creditDebitValue = 0;
-                                                        console.log("fieldObj.field_value :: " + fieldObj.field_value);
+                                                        console.log("fieldObj.field_value :: " , fieldObj.field_value);
                                                         
                                                         let data = fieldObj.field_value;
                                                         console.log('DATA : ', data);
+                                                        console.log('typeof data : ', typeof data);
+
+                                                        if(typeof data === 'string') {
+                                                            data = JSON.parse(data);
+                                                        }
+
+                                                        console.log('DATA.transaction_data : ', data.transaction_data);
                                                         console.log("data.transaction_data.transaction_type_id :: " , data.transaction_data.transaction_type_id);
 
-                                                        console.log("fieldObj.field_value.transaction_data.transaction_type_id :: " , fieldObj.field_value.transaction_data.transaction_type_id);
-                                                        fieldObj.field_value.transaction_data.transaction_type_id == 1? creditDebitValue = fieldObj.field_value.transaction_data.transaction_amount: creditDebitValue = fieldObj.field_value.transaction_data.transaction_amount;
+                                                        //console.log("fieldObj.field_value.transaction_data.transaction_type_id :: " , fieldObj.field_value.transaction_data.transaction_type_id);
+                                                        //fieldObj.field_value.transaction_data.transaction_type_id == 1? creditDebitValue = fieldObj.field_value.transaction_data.transaction_amount: creditDebitValue = fieldObj.field_value.transaction_data.transaction_amount;                                                        
+                                                        data.transaction_data.transaction_type_id == 1? creditDebitValue = data.transaction_data.transaction_amount: creditDebitValue = data.transaction_data.transaction_amount;
 
                                                         activityCommonService.analyticsUpdateWidgetValue(request, request.activity_id, 0, creditDebitValue);
                                                         next();
