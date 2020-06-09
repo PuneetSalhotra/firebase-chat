ff40f06c9469dc081fe385af2d4daaae54bb7c2c
ajay <ajayp@athmin.com>
2020-06-09 13:54:32 +0530

    convert s3url pdf to base64

--- server/docusign/services/docusignService.js
+++ server/docusign/services/docusignService.js
@@ -1,3 +1,4 @@
+const pdf2base64 = require('pdf-to-base64');
 const docusign = require('docusign-esign')
     , path = require('path')
     , fs = require('fs')
@@ -6,6 +7,8 @@ const docusign = require('docusign-esign')
     , express = require('express')
     , envir = process.env
     ;
+const AWS = require('aws-sdk');
+var extract = require('pdf-text-extract')
 
 function CommonDocusignService(objectCollection) {
     const util = objectCollection.util;
@@ -16,11 +19,12 @@ function CommonDocusignService(objectCollection) {
 
 this.addFile = async  (request, res) => {
         // const qp =request.query;
-  const accessToken =  'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjY4MTg1ZmYxLTRlNTEtNGNlOS1hZjFjLTY4OTgxMjIwMzMxNyJ9.eyJUb2tlblR5cGUiOjUsIklzc3VlSW5zdGFudCI6MTU5MTU5OTYwNCwiZXhwIjoxNTkxNjI4NDA0LCJVc2VySWQiOiIxNzkxNTk5Yy0xOTQ4LTQ1Y2YtODhjYy01ZTI2NjY1ZWI5YWEiLCJzaXRlaWQiOjEsInNjcCI6WyJzaWduYXR1cmUiLCJjbGljay5tYW5hZ2UiLCJvcmdhbml6YXRpb25fcmVhZCIsInJvb21fZm9ybXMiLCJncm91cF9yZWFkIiwicGVybWlzc2lvbl9yZWFkIiwidXNlcl9yZWFkIiwidXNlcl93cml0ZSIsImFjY291bnRfcmVhZCIsImRvbWFpbl9yZWFkIiwiaWRlbnRpdHlfcHJvdmlkZXJfcmVhZCIsImR0ci5yb29tcy5yZWFkIiwiZHRyLnJvb21zLndyaXRlIiwiZHRyLmRvY3VtZW50cy5yZWFkIiwiZHRyLmRvY3VtZW50cy53cml0ZSIsImR0ci5wcm9maWxlLnJlYWQiLCJkdHIucHJvZmlsZS53cml0ZSIsImR0ci5jb21wYW55LnJlYWQiLCJkdHIuY29tcGFueS53cml0ZSJdLCJhdWQiOiJmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgiLCJhenAiOiJmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgiLCJpc3MiOiJodHRwczovL2FjY291bnQtZC5kb2N1c2lnbi5jb20vIiwic3ViIjoiMTc5MTU5OWMtMTk0OC00NWNmLTg4Y2MtNWUyNjY2NWViOWFhIiwiYW1yIjpbImludGVyYWN0aXZlIl0sImF1dGhfdGltZSI6MTU5MTU5OTYwMSwicHdpZCI6IjRlNmYxMTVmLTNkMzAtNGJjMy04OTE0LTkzMjk5NTJhYmZkYiJ9.nGDi-2TNOn4O6x_200RRYwCKThSgvXLMbLGNAs54vYMDZ_v4ejkUtJ5VAk7f30az5zxC5MjICRFqLHOjscqQZBKx1ofnFCbTXQjAL6A_RV_6qWVtNgMAXUv5e7j6iWRj-TIoPjZi6Azp3FqdwGpnCD7fPSw0Iiz_zoKra7-j1oxTBipyy34pnz4TBs21l-Qoqhxfu9zpu_zptSYxO9OYrLLP8t_GligZAMEUreXAJ30T3cdVainFCOqUD597Li1ZIobXIFaVkF4Zc97nIIA6FjueICPGan0vtGcVBBQ76xQtMGeoYUlHeqBx_RZz2MBQk1cAavpLcAgFxyz_CiBYvA';
+  const accessToken =  'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjY4MTg1ZmYxLTRlNTEtNGNlOS1hZjFjLTY4OTgxMjIwMzMxNyJ9.eyJUb2tlblR5cGUiOjUsIklzc3VlSW5zdGFudCI6MTU5MTY4Mzc2NSwiZXhwIjoxNTkxNzEyNTY1LCJVc2VySWQiOiIxNzkxNTk5Yy0xOTQ4LTQ1Y2YtODhjYy01ZTI2NjY1ZWI5YWEiLCJzaXRlaWQiOjEsInNjcCI6WyJzaWduYXR1cmUiLCJjbGljay5tYW5hZ2UiLCJvcmdhbml6YXRpb25fcmVhZCIsInJvb21fZm9ybXMiLCJncm91cF9yZWFkIiwicGVybWlzc2lvbl9yZWFkIiwidXNlcl9yZWFkIiwidXNlcl93cml0ZSIsImFjY291bnRfcmVhZCIsImRvbWFpbl9yZWFkIiwiaWRlbnRpdHlfcHJvdmlkZXJfcmVhZCIsImR0ci5yb29tcy5yZWFkIiwiZHRyLnJvb21zLndyaXRlIiwiZHRyLmRvY3VtZW50cy5yZWFkIiwiZHRyLmRvY3VtZW50cy53cml0ZSIsImR0ci5wcm9maWxlLnJlYWQiLCJkdHIucHJvZmlsZS53cml0ZSIsImR0ci5jb21wYW55LnJlYWQiLCJkdHIuY29tcGFueS53cml0ZSJdLCJhdWQiOiJmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgiLCJhenAiOiJmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgiLCJpc3MiOiJodHRwczovL2FjY291bnQtZC5kb2N1c2lnbi5jb20vIiwic3ViIjoiMTc5MTU5OWMtMTk0OC00NWNmLTg4Y2MtNWUyNjY2NWViOWFhIiwiYW1yIjpbImludGVyYWN0aXZlIl0sImF1dGhfdGltZSI6MTU5MTY4Mzc2MSwicHdpZCI6IjRlNmYxMTVmLTNkMzAtNGJjMy04OTE0LTkzMjk5NTJhYmZkYiJ9.PQmdRKOfRdVH9hyozQDC07LSz8VcrjjdAsHnvCkfK7FF9eMyh2NP_cHQiMT_TpLeeUg5KD6_F6EqJXAbg_UZKT1JfvS3rGLTSFy_CXHdscYOgIBsEY3Y0TWn4L28FSqp2Ya6pxXIyGwXUOY6GuRVqGjQ9Y5dh_xrTrMvRmd7Fw5tQxbnvTZvfGKeJPlF4WlruJU0Gn7Wxh-XR23SR8EZ3rzL_FLiENnnJ4KIdA7tMCXYODiozYGqWcPHaLqBtMtyAzVsT-bvKXDWPXdj8IDBmQiOctxNn0sXAHOR4wAYQQauXQxW5TeGQD9eBJ_i6OnHASH2hCuolwGXEQRfoS01pw';
   const accountId =  global.config.accountId; 
-  const signerName = 'ajay';
-  const signerEmail = 'ajayp@athmin.com';
+  const signerName =  request.receiver_name || 'ajay' ;
+  const signerEmail = request.receiver_email || 'ajayp@athmin.com' ;
   const fileName = 'ajay.pdf';
+//   const fileName = testurl(request,request.url_path)
   const apiClient = new docusign.ApiClient();
   apiClient.setBasePath(basePath);
   apiClient.addDefaultHeader('Authorization', 'Bearer ' + accessToken);
@@ -31,15 +35,20 @@ this.addFile = async  (request, res) => {
   // Start with the request object
   const envDef = new docusign.EnvelopeDefinition();
   //Set the Email Subject line and email message
-  envDef.emailSubject = global.config.documentTypes.customerApplicationForm.emailSubject || 'Please sign this document sent from the Node example';
+  envDef.emailSubject =  request.subject || global.config.documentTypes.customerApplicationForm.emailSubject || 'Please sign this document sent from the Node example';
   envDef.emailBlurb = global.config.documentTypes.customerApplicationForm.emailBlurb || 'Please sign this document sent from the Node example.'
   // Read the file from the document and convert it to a Base64String
-  const pdfBytes = fs.readFileSync(path.resolve(__dirname, fileName))
-      , pdfBase64 = pdfBytes.toString('base64');
+//   const pdfBytes = fs.readFileSync(path.resolve(__dirname, fileName))
+    //   , pdfBase64 = pdfBytes.toString('base64');
+      try{
+        var pdfBase64 =await getPdftoBase64(request,request.url_path)
+      }catch (e){
+          console.log(e)
+      }
   // Create the document request object
   const doc = docusign.Document.constructFromObject({documentBase64: pdfBase64,
         fileExtension: 'pdf',  // You can send other types of documents too.
-        name: 'ajay.pdf', documentId: '1'});
+        name: 'test.pdf', documentId: '1'});
   // Create a documents object array for the envelope definition and add the doc object
   envDef.documents = [doc];
 
@@ -94,14 +103,26 @@ this.addFile = async  (request, res) => {
         results[0] = await db.callDBProcedure(request, 'ds_v1_activity_docusign_mapping_insert', paramsArray, 0);
         var response = {'activity_document_id':results[0][0]['activity_document_id']}
             return response
-  // Envelope has been created:
-//     res.send (`<html lang="en"><body>
-//                 <h3>Envelope Created!</h3>
-//                 <p>Signer: ${signerName} &lt;${signerEmail}&gt;</p>
-//                 <p>Results</p><p><pre><code>${JSON.stringify(results, null, 4)}</code></pre></p>`);
   }
 }
 
 
+async function getPdftoBase64(request,S3Url){
+var pdfBase64 = ''
+await pdf2base64(S3Url)
+    .then(
+        (response) => {
+            pdfBase64 = response
+        }
+    )
+    .catch(
+        (error) => {
+            console.log(error); //Exepection error....
+        }
+    )
+    return pdfBase64
+}
 };
+
+
 module.exports = CommonDocusignService;
