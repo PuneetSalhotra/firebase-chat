d389c8d160be81dba5ebede143fa8171b413f613
ajay <ajayp@athmin.com>
2020-06-15 03:09:39 +0530

    integrated hookup funtionality and update status in db

--- server/docusign/controllers/docusignController.js
+++ server/docusign/controllers/docusignController.js
@@ -3,13 +3,15 @@ var CommonDocusignService = require("../services/docusignService");
 function docusignController(objCollection) {
   var responseWrapper = objCollection.responseWrapper;
   var app = objCollection.app;
+  const xmlparser = require('express-xml-bodyparser');
+app.use(xmlparser());
   const commonDocusignService = new CommonDocusignService(objCollection);
 
   app.post(
     '/' + global.config.version + '/docusign/document/add',
     async (req, res) => {
       try {
-        let result = await commonDocusignService.addFile(req.body, res);
+         let result = await commonDocusignService.addFile(req.body, res);
         // res.send(responseWrapper.getResponse(false, result, 200, req.body));
       } catch (err) {
         res.send(responseWrapper.getResponse(err, {}, -9998, req.body));
