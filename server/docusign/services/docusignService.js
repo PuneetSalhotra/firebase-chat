d60e26d23e588f44ea25e79d5e765de06e8924ef
ajay <ajayp@athmin.com>
2020-06-18 10:42:05 +0530

    removed junk data

--- server/docusign/services/docusignService.js
+++ server/docusign/services/docusignService.js
@@ -22,7 +22,7 @@ function commonDocusignService(objectCollection) {
   var ActivityTimelineService = require("../../services/activityTimelineService.js");
   const activityTimelineService = new ActivityTimelineService(objectCollection);
   this.addFile = async (request, res) => {
-    // generate refresh token
+
     await getAccessTokenUsingRefreshToken(accessToken => {
       const accountId = global.config.accountId;
       const signerName = request.receiver_name;
@@ -30,45 +30,38 @@ function commonDocusignService(objectCollection) {
       const apiClient = new docusign.ApiClient();
       apiClient.setBasePath(basePath);
       apiClient.addDefaultHeader('Authorization', 'Bearer ' + accessToken);
-      // Set the DocuSign SDK components to use the apiClient object
       docusign.Configuration.default.setDefaultApiClient(apiClient);
 
-      // Create the envelope request
       const envDef = new docusign.EnvelopeDefinition();
-      //Set the Email Subject line and email message
       envDef.emailSubject = request.subject || global.config.documentTypes.customerApplicationForm.emailSubject;
       if (request.hasOwnProperty('document_type') && global.config.documentTypes.hasOwnProperty(request.document_type)) {
         envDef.emailBlurb = global.config.documentTypes[request.document_type]['emailBlurb'];
       }
-      // Read the file from the document and convert it to a Base64String
+
       getHtmlToBase64(request, res).then(async pdfResult => {
         const s3UploadUrl = await uploadReadableStreamOnS3(request, pdfResult['pdf'], res)
-        // Create the document request object
         const doc = docusign.Document.constructFromObject({
           documentBase64: pdfResult['pdfBase64'],
-          fileExtension: 'pdf', // You can send other types of documents too.
-          name: 'test.pdf',
+          fileExtension: 'pdf',
+          name: Date.now()+'.pdf',
           documentId: '1'
         });
-        // Create a documents object array for the envelope definition and add the doc object
         envDef.documents = [doc];
 
-        // Create the signer object with the previously provided name / email address
         const signer = docusign.Signer.constructFromObject({
           name: signerName,
           email: signerEmail,
           routingOrder: '1',
           recipientId: '1'
         });
-        // Create the signHere tab to be placed on the envelope
+
         var signHere;
         if (request.hasOwnProperty('document_type') && global.config.documentTypes.hasOwnProperty(request.document_type))
           signHere = global.config.documentTypes[request.document_type]['signHereTabs'];
         else
           signHere = global.config.documentTypes['customerApplicationForm']['signHereTabs'];
-        // Create the overall tabs object for the signer and add the signHere tabs array
-        // Note that tabs are relative to receipients/signers.
-        try {
+
+          try {
           signer.tabs = docusign.Tabs.constructFromObject({
             signHereTabs: signHere
           });
@@ -76,15 +69,12 @@ function commonDocusignService(objectCollection) {
           console.log(e)
           return res.send(responseWrapper.getResponse(e, {}, -9998, request));
         }
-        // Add the recipients object to the envelope definition.
-        // It includes an array of the signer objects. 
+
         envDef.recipients = docusign.Recipients.constructFromObject({
           signers: [signer]
         });
-        // Set the Envelope status. For drafts, use 'created' To send the envelope right away, use 'sent'
-        envDef.status = 'sent';
 
-        // Send the envelope
+        envDef.status = 'sent';
         let envelopesApi = new docusign.EnvelopesApi(),
           results;
         var eventNotification = {
