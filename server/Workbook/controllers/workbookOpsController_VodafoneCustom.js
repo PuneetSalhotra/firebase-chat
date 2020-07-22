c35301fa3a58e4eacac9c59de986e058af8e7404
nani <nani@nani-Vostro-15-3568>
2020-07-22 17:38:38 +0530

    Fixed Workbook Bots - Account Manager Name

--- server/Workbook/controllers/workbookOpsController_VodafoneCustom.js
+++ server/Workbook/controllers/workbookOpsController_VodafoneCustom.js
@@ -22,7 +22,26 @@ function WorkbookOpsController_VodafoneCustom(objCollection) {
     const activityCommonService = objCollection.activityCommonService;
 
     const workbookOpsService_VodafoneCustom = new WorkbookOpsService_VodafoneCustom(objCollection);
+   
+    app.post('/' + global.config.version + '/excel/s3/upload', async (req, res) => {        
+        const [err, responseData] = await workbookOpsService_VodafoneCustom.uploadReadableStreamToS3Method(req.body);        
+        if (!err) {
+            res.send(responseWrapper.getResponse(false, responseData, 200, req.body));
+        } else {
+            console.log("/excel/s3/upload | Error: ", err);
+            res.send(responseWrapper.getResponse(err, { message: err }, -9998, req.body));
+        }
+    });
 
+    app.post('/' + global.config.version + '/excel/s3/download', async (req, res) => {
+        const [err, responseData] = await workbookOpsService_VodafoneCustom.downloadExcelFromS3(req.body);        
+        if (!err) {
+            res.send(responseWrapper.getResponse(false, responseData, 200, req.body));
+        } else {
+            console.log("/excel/s3/download | Error: ", err);
+            res.send(responseWrapper.getResponse(err, { message: err }, -9998, req.body));
+        }
+    });
 
     // Helper methods
     function sleep(ms) {
@@ -58,7 +77,7 @@ function WorkbookOpsController_VodafoneCustom(objCollection) {
         logger.silly("[timeout_error]: %j", error.message, { type: 'excel_sqs_consumer', error: serializeError(error) });
     });
 
-    sqsConsumerApp.start();
+    //sqsConsumerApp.start();
     logger.silly("sqsConsumerApp started", { type: 'excel_sqs_consumer' });
 }
 
