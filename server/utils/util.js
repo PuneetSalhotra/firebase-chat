9db579dc80c9cc4a450eed5f7c66e8b25d52573a
nani <nani@nani-Vostro-15-3568>
2020-09-29 14:32:37 +0530

    suppressing org level pubnub pushes

--- server/utils/util.js
+++ server/utils/util.js
@@ -2075,18 +2075,18 @@ function Util(objectCollection) {
         ) {
             return [true, {
                 message: "Incorrect target workforce_id specified."
-            }]
+            }];
         }
 
-        pubnubWrapper.publish(request.target_workforce_id, {
-            type: "workforce_push",
-            organization_id: Number(request.organization_id),
-            activity_type_category_id: 0,
-            activity_id: 0,
-            activity_title: request.push_title,
-            description: request.push_message,
-            target_workforce_id:request.target_workforce_id
-        });
+        //pubnubWrapper.publish(request.target_workforce_id, {
+        //    type: "workforce_push",
+        //    organization_id: Number(request.organization_id),
+        //    activity_type_category_id: 0,
+        //    activity_id: 0,
+        //    activity_title: request.push_title,
+        //    description: request.push_message,
+        //    target_workforce_id:request.target_workforce_id
+        //});
 
         return [error, {
             message: `Push sent to ${request.target_workforce_id}`
