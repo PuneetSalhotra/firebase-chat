a2a5bc8c39d93044f6b76325fc3d5e0120eff245
shankar1525 <55125365+shankar1525@users.noreply.github.com>
2020-10-29 17:24:02 +0530

    type change

--- server/analytics/controllers/analyticsController.js
+++ server/analytics/controllers/analyticsController.js
@@ -274,7 +274,7 @@ function AnalyticsController(objCollection)
     app.post('/' + global.config.version + '/analytics/widget-type/list', async (req, res) => {        
             
                 let [err,result] = await analyticsService.selectWidgetType(req.body);
-              if(err){
+              if(!err){
                 res.send(responseWrapper.getResponse(false, result, 200, req.body));
             } else {
                 res.send(responseWrapper.getResponse(err, {}, -9998, req.body));
