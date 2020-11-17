dd9c945f7438d71821ee44a608501d7fcc24a0a3
nani <nani@nani-Vostro-15-3568>
2020-11-17 22:21:47 +0530

    modified build yml scripts

--- deployment/start_services_sprint.sh
+++ deployment/start_services_sprint.sh
@@ -4,7 +4,7 @@ then
   cd /sprint_desker_api/
   #We are removing and freshly installing coz aspose java dependency
   rm -rf node_modules
-  npm install
+  npm install --production
   pm2 delete /^Sprint_ConsumerZero/
   pm2 delete /^Sprint_Main/
   pm2 reload sprintProcesses.yml
