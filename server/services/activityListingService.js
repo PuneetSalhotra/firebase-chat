0dceec485b133ffbedeacdb32af74f8ebfaa849d
Ben Sooraj <ben@grenerobotics.com>
2020-11-26 12:08:53 +0530

    dummy commit 2

--- server/services/activityListingService.js
+++ server/services/activityListingService.js
@@ -3520,7 +3520,7 @@ async function processFormInlineDataV1(request, data){
 		const [errorZero, summaryData] = await self.getActBulkSummaryData(request);
 		if (errorZero) { error = errorZero };
 
-		// Get the sequencing and display name
+		// Get the sequencing & display name
 		try {
 			// headerMetadata = await cacheWrapper.getEmailProvider();
 			headerMetadata = bulkFeasibilitySummarySheetHeaderConfig;
