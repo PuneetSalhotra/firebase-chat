44fafcd3a2844cb3de2660f74bbdd3e75c25d73c
nani <nani@nani-Vostro-15-3568>
2021-01-15 16:35:40 +0530

    1) Added the following services 		1. /drs/doc-repo/access/set 		2. /drs/doc-repo/specific-role/access/set 		3. /drs/doc-repo/specific-role/access/reset 		4. /drs/doc-repo/add  	2) Returning the DRS related columns in the Asset Details Service

--- server/DocumentRepositorySystem/controllers/drsController.js
+++ server/DocumentRepositorySystem/controllers/drsController.js
@@ -6,8 +6,10 @@ function DrsController(objCollection)
     const responseWrapper = objCollection.responseWrapper;
     const app = objCollection.app;
 
-    app.post('/' + global.config.version + '/drs/sample/api', async (req, res) => {        
-        const [err, data] = await drsService.sampleFunc(req.body);
+    
+    //Service to update the access to document repository
+    app.post('/' + global.config.version + '/drs/doc-repo/access/set', async (req, res) => {        
+        const [err, data] = await drsService.updateAccessToDocRepo(req.body);
         if (!err) {
             res.send(responseWrapper.getResponse({}, data, 200, req.body));
         } else {
@@ -15,6 +17,43 @@ function DrsController(objCollection)
         }
         res.send(responseWrapper.getResponse({}, [], 200, req.body));
     });
+
+
+    //Service to share document repository to a specific role
+    app.post('/' + global.config.version + '/drs/doc-repo/specific-role/access/set', async (req, res) => {
+        const [err, data] = await drsService.shareDRSToASpecificRole(req.body);
+        if (!err) {
+            res.send(responseWrapper.getResponse({}, data, 200, req.body));
+        } else {
+            res.send(responseWrapper.getResponse(err, data, -9999, req.body));
+        }
+        res.send(responseWrapper.getResponse({}, [], 200, req.body));
+    });
+
+    //Service to remove sharing of a document repository to a specific role
+    app.post('/' + global.config.version + '/drs/doc-repo/specific-role/access/reset', async (req, res) => {
+        const [err, data] = await drsService.removeDRSToASpecificRole(req.body);
+        if (!err) {
+            res.send(responseWrapper.getResponse({}, data, 200, req.body));
+        } else {
+            res.send(responseWrapper.getResponse(err, data, -9999, req.body));
+        }
+        res.send(responseWrapper.getResponse({}, [], 200, req.body));
+    });
+
+    
+    //Service to create a folder in document repository
+    app.post('/' + global.config.version + '/drs/doc-repo/add', async (req, res) => {
+        const [err, data] = await drsService.createFolder(req.body);
+        if (!err) {
+            res.send(responseWrapper.getResponse({}, data, 200, req.body));
+        } else {
+            res.send(responseWrapper.getResponse(err, data, -9999, req.body));
+        }
+        res.send(responseWrapper.getResponse({}, [], 200, req.body));
+    });
+
+    //Service to create a new folder in s3 and save it into document repository
 
 }
 
