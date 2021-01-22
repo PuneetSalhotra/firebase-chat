bdddde11bcefb6f8e031c24bc898ef12d025ad3f
shankar1525 <55125365+shankar1525@users.noreply.github.com>
2021-01-22 11:47:41 +0530

    history inserts

--- server/DocumentRepositorySystem/services/drsService.js
+++ server/DocumentRepositorySystem/services/drsService.js
@@ -56,8 +56,9 @@ function DrsService(objectCollection) {
     const queryString = util.getQueryString('ds_p1_document_repository_asset_mapping_insert', paramsArr);
     if (queryString !== '') {
         await db.executeQueryPromise(0, queryString, request)
-            .then((data) => {
+            .then(async (data) => {
               responseData = data;
+              let [assetHisErr,assetHisData] = await assetHistoryInsert(request,2406)
                 error = false;
             })
             .catch((err) => {
@@ -84,8 +85,9 @@ function DrsService(objectCollection) {
     const queryString = util.getQueryString('ds_p1_document_repository_asset_mapping_delete', paramsArr);
     if (queryString !== '') {
         await db.executeQueryPromise(0, queryString, request)
-            .then((data) => {
+            .then(async (data) => {
               responseData = data;
+              let [assetHisErr,assetHisData] = await assetHistoryInsert(request,2407)
                 error = false;
             })
             .catch((err) => {
@@ -183,8 +185,10 @@ function DrsService(objectCollection) {
     const queryString = util.getQueryString('ds_p1_document_repository_list_insert', paramsArr);
     if (queryString !== '') {
         await db.executeQueryPromise(0, queryString, request)
-            .then((data) => {
+            .then(async (data) => {
               responseData = data;
+
+              let [historyErr,historyData] = await docHistoryInsert({...request,document_repository_id:data[0].document_repository_id},2403)
                 error = false;
             })
             .catch((err) => {
@@ -195,8 +199,56 @@ function DrsService(objectCollection) {
     return [error, responseData];
   };
 
+  async function docHistoryInsert(request,update_type_id){
+      let responseData=[];
+      let error = true;
+    let paramsArr = [
+        request.organization_id,
+        request.document_repository_id,
+        update_type_id||0,
+        util.getCurrentUTCTime()
+    ];
+    const queryString = util.getQueryString('ds_p1_document_repository_list_history_insert', paramsArr);
+    if (queryString !== '') {
 
-    this.updateDRSForTag = async (request) => {
+        await db.executeQueryPromise(1, queryString, request)
+            .then((data) => {
+                responseData = data;
+                error = false;
+            })
+            .catch((err) => {
+                error = err;
+            });
+    }
+    return [responseData,error]
+  }
+
+  async function assetHistoryInsert(request,update_type_id){
+    let responseData=[];
+    let error = true;
+    let paramsArr = [
+        request.organization_id,
+        request.document_repository_id,
+        request.target_asset_id,
+        update_type_id||0,
+        util.getCurrentUTCTime()
+    ];
+    const queryString = util.getQueryString('ds_p1_document_repository_asset_mapping_history_insert', paramsArr);
+    if (queryString !== '') {
+  
+        await db.executeQueryPromise(1, queryString, request)
+            .then((data) => {
+                responseData = data;
+                error = false;
+            })
+            .catch((err) => {
+                error = err;
+            });
+    }
+    return [responseData,error]
+  }
+
+  this.updateDRSForTag = async (request) => {
         let responseData = [],
             error = true;
 
@@ -211,8 +263,9 @@ function DrsService(objectCollection) {
         if (queryString !== '') {
 
             await db.executeQueryPromise(1, queryString, request)
-                .then((data) => {
+                .then(async(data) => {
                     responseData = data;
+                    let [historyErr,historyData] = await docHistoryInsert(request,2405)
                     error = false;
                 })
                 .catch((err) => {
@@ -221,7 +274,7 @@ function DrsService(objectCollection) {
         }
 
         return [error, responseData];
-    };
+  };
 
 
     this.selectDRSAsset = async (request) => {
@@ -414,8 +467,9 @@ function DrsService(objectCollection) {
         if (queryString !== '') {
 
             await db.executeQueryPromise(0, queryString, request)
-                .then((data) => {
+                .then(async (data) => {
                     responseData = data;
+                    let [historyErr,historyData] = await docHistoryInsert(request,2404)
                     error = false;
                 })
                 .catch((err) => {
