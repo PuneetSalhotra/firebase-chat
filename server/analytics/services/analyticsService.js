e50599f55595baf015b15223557acc83f55116f1
rajendra <rajendra@thelattice.in>
2021-02-03 14:23:57 +0530

    Functionality to get asset account target list

--- server/analytics/services/analyticsService.js
+++ server/analytics/services/analyticsService.js
@@ -2738,6 +2738,34 @@ function AnalyticsService(objectCollection)
         }
         return [error, responseData];
     }
+    //Functionality to get asset account target list          
+    this.getAssetAccountTargetList = async (request) => {
+
+        let responseData = [],
+            error = true;
+        
+        const paramsArr = [     
+              request.organization_id,
+              request.target_asset_id,
+              request.widget_timescale || '',
+              request.page_start || 0,
+              request.page_limit || 100
+        ];
+
+        const queryString = util.getQueryString('ds_v1_vil_asset_account_target_mapping_select', paramsArr);
+        if (queryString !== '') {
+            await db.executeQueryPromise(1, queryString, request)
+              .then((data) => {
+                  responseData = data;
+                  error = false;
+              })
+              .catch((err) => {
+                  error = err;
+              })
+        }
+
+        return [error, responseData];
+    }
 
 }
 
