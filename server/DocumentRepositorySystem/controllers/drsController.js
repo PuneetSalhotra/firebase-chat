98487fda212ce674ebe13fae7046d6c4df9bcb85
shankar1525 <55125365+shankar1525@users.noreply.github.com>
2021-03-10 16:57:05 +0530

    docs api and assets inline..

--- server/DocumentRepositorySystem/controllers/drsController.js
+++ server/DocumentRepositorySystem/controllers/drsController.js
@@ -185,6 +185,15 @@ function DrsController(objCollection)
         }
     });
 
+    app.post('/' + global.config.version + '/drs/doc-repo/workforce/delete', async (req, res) => {
+        const [err, data] = await drsService.deleteDocRepoForWorkforce(req.body);
+        if (!err) {
+            res.send(responseWrapper.getResponse({}, data, 200, req.body));
+        } else {
+            res.send(responseWrapper.getResponse(err, data, -9999, req.body));
+        }
+    });
+
 }
 
 module.exports = DrsController;
