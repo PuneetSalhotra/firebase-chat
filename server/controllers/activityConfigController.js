dbff93602bf345292c9a1f563fbe7a0ac5d1e038
shankar1525 <55125365+shankar1525@users.noreply.github.com>
2021-04-06 15:49:06 +0530

    enter pan to elastic

--- server/controllers/activityConfigController.js
+++ server/controllers/activityConfigController.js
@@ -219,6 +219,17 @@ function ActivityConfigController(objCollection) {
             res.send(responseWrapper.getResponse(err, data, -9999, req.body));
         }
     });
+
+    //adding pan and activity details to elastic 
+    app.post('/' + global.config.version + '/activity/pan/elastic/entry', async (req, res) => {
+        const [err, data] = await activityConfigService.panElasticEntry(req.body);
+        if (!err) {
+            res.send(responseWrapper.getResponse({}, data, 200, req.body));
+        } else {
+            console.log('Error - /activity/pan/elastic/entry : ', err);
+            res.send(responseWrapper.getResponse(err, data, -9999, req.body));
+        }
+    });
 }
 
 
