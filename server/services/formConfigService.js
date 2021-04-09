e962097cd47fab99d54d7724b4ca9140db627e10
sravan7777 <sravan@blueflock.com>
2021-04-10 01:24:58 +0530

    field alter timeline text alter

--- server/services/formConfigService.js
+++ server/services/formConfigService.js
@@ -536,7 +536,7 @@ function FormConfigService(objCollection) {
         console.log('newData from Request: ', newData);
         request.new_field_value = newData.field_value;
         var dataTypeId = Number(newData.field_data_type_id);
-
+        //let dataTypeCategoryId = Number(newData.field_data_type_category_id);
         //Listener to update data in Intermediate tables for Reference/combo bots
         switch(Number(newData.field_data_type_id)) {
             case 57: fireBotUpdateIntTables(request, newData);
@@ -632,10 +632,16 @@ function FormConfigService(objCollection) {
                     request.activity_inline_data = JSON.stringify(retrievedInlineData);
                     //console.log('oldFieldValue: ', oldFieldValue);
                     let content = '';
-                    if (String(oldFieldValue).trim().length === 0) {
-                        content = `In the ${newData.form_name}, the field ${newData.field_name} was updated to ${newFieldValue}`;
-                    } else {
-                        content = `In the ${newData.form_name}, the field ${newData.field_name} was updated from ${oldFieldValue} to ${newFieldValue}`;
+                    let simpleDataTypes = [1,2,3,7,8,9,10,14,15,19,21,22];
+                    console.log("/activity/form/alter data_type_category_id "+newData.field_data_type_category_id+" exists in simple categories : "+simpleDataTypes.includes(newData.field_data_type_category_id));
+                    if(simpleDataTypes.includes(newData.field_data_type_category_id)){
+                        if (String(oldFieldValue).trim().length === 0) {
+                            content = `In the ${newData.form_name}, the field ${newData.field_name} was updated to ${newFieldValue}`;
+                        } else {
+                            content = `In the ${newData.form_name}, the field ${newData.field_name} was updated from ${oldFieldValue} to ${newFieldValue}`;
+                        }
+                    }else{
+                        content = `In the ${newData.form_name}, the field ${newData.field_name} was updated`;
                     }
 
                     let activityTimelineCollection = {
