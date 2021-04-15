ab9fa6a5bb6406ba881309922f49977938239205
sravan7777 <sravan@blueflock.com>
2021-04-15 20:28:58 +0530

    new flags for asset inline collection

--- server/services/assetService.js
+++ server/services/assetService.js
@@ -827,7 +827,9 @@ function AssetService(objectCollection) {
             //email integrations
             "email_integration_enable":util.replaceDefaultNumber(rowArray[0]['email_integration_enable']),
             "asset_linked_enabled" :util.replaceDefaultNumber(rowArray[0]['asset_linked_enabled ']),
-            "asset_linked_status_datetime":util.replaceDefaultDatetime(rowArray[0]['asset_linked_status_datetime '])
+            "asset_linked_status_datetime":util.replaceDefaultDatetime(rowArray[0]['asset_linked_status_datetime ']),
+            "asset_flag_organization_management":util.replaceDefaultNumber(rowArray[0]['asset_flag_organization_management']),
+            "asset_admin_access_data" :util.replaceDefaultString(rowArray[0]['asset_admin_access_data '])
         };
 
         callback(false, rowData);
