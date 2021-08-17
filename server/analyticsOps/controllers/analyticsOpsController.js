0731debf7148c6cedf3f96987bf66dd15db9cacd
PuneetSalhotra <puneetsalhotra3@gmail.com>
2021-08-17 14:09:52 +0530

    apis added

--- server/analyticsOps/controllers/analyticsOpsController.js
+++ server/analyticsOps/controllers/analyticsOpsController.js
@@ -74,6 +74,51 @@ function AnalyticsOpsController (objCollection) {
 
     app.post
     (
+        '/' + global.config.version + '/analytics/widget/drilldown/header/mapping/insert',
+        async (req, res) => {
+            try {
+                let result = await analyticsOpsService.insertWidgetDrilldownHeaderMapping(req.body);
+                res.send(responseWrapper.getResponse(false, result, 200, req.body));
+            }
+            catch (err) {
+                console.log("Error  in /analytics/widget/filter/master/select", err, err.stack);
+                res.send(responseWrapper.getResponse(err, {}, -9998, req.body));
+            }
+        }
+    );
+
+    app.post
+    (
+        '/' + global.config.version + '/analytics/workforce/activity_type/mapping/update',
+        async (req, res) => {
+            try {
+                let result = await analyticsOpsService.updateWorkforceActivityTypeMapping(req.body);
+                res.send(responseWrapper.getResponse(false, result, 200, req.body));
+            }
+            catch (err) {
+                console.log("Error  in /analytics/widget/filter/master/select", err, err.stack);
+                res.send(responseWrapper.getResponse(err, {}, -9998, req.body));
+            }
+        }
+    );
+
+    app.post
+    (
+        '/' + global.config.version + '/analytics/widget/drilldown/header/select',
+        async (req, res) => {
+            try {
+                let result = await analyticsOpsService.selectWidgetDrilldownHeaderMapping(req.body);
+                res.send(responseWrapper.getResponse(false, result, 200, req.body));
+            }
+            catch (err) {
+                console.log("Error  in /analytics/widget/filter/master/select", err, err.stack);
+                res.send(responseWrapper.getResponse(err, {}, -9998, req.body));
+            }
+        }
+    );
+
+    app.post
+    (
         '/' + global.config.version + '/analytics/report/filter/insert',
         async (req, res) => {
             try {
@@ -89,3 +134,4 @@ function AnalyticsOpsController (objCollection) {
 }
 
 module.exports = AnalyticsOpsController;
+    
--- server/analyticsOps/services/analyticsOpsService.js
+++ server/analyticsOps/services/analyticsOpsService.js
@@ -160,6 +160,81 @@ function AnalyticsOpsService(objectCollection)
             return Promise.reject(error);
         }
     };
+
+
+    this.insertWidgetDrilldownHeaderMapping =
+    async (request) => {
+        try {
+            let paramsArray;
+            
+            paramsArray =
+                new Array
+                    (
+                        request.tag_type_id,
+                        request.header_id,
+                        request.sequence_id,
+                        request.conversion_format,
+                        request.organization_id,
+                        request.log_asset_id,
+                        util.getCurrentUTCTime()
+                    );
+
+            await db.callDBProcedureR2(request, 'ds_p1_widget_drilldown_header_mapping_insert', paramsArray, 0);
+
+            return {};
+        }
+        catch (error) {
+            return Promise.reject(error);
+        }
+    };
+
+    this.updateWorkforceActivityTypeMapping =
+    async (request) => {
+        try {
+            let paramsArray;
+            
+            paramsArray =
+                new Array
+                    (
+                        request.organization_id,
+                        request.account_id,
+                        request.workforce_id,
+                        request.activity_type_id,
+                        request.workflow_fields,
+                        request.log_asset_id,
+                        util.getCurrentUTCTime()
+                    );
+
+            await db.callDBProcedureR2(request, 'ds_p1_workforce_activity_type_mapping_update_workflow_fields', paramsArray, 0);
+
+            return {};
+        }
+        catch (error) {
+            return Promise.reject(error);
+        }
+    };
+
+    this.selectWidgetDrilldownHeaderMapping =
+    async (request) => {
+        try {
+            let paramsArray;
+            
+            paramsArray =
+                new Array
+                    (
+                        request.organization_id,
+                        request.start_from,
+                        request.limit_value
+                    );
+
+            let result = await db.callDBProcedureR2(request, 'ds_p1_widget_drilldown_header_master_select', paramsArray, 1);
+
+            return result;
+        }
+        catch (error) {
+            return Promise.reject(error);
+        }
+    };
 }
 
 module.exports = AnalyticsOpsService;
