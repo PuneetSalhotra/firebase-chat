90267846a44a81a7dcef6f3f184e7a4501667b9d
sravan7777 <sravan@blueflock.com>
2021-08-26 11:12:52 +0530

    adding mail config to preprod also

--- server/utils/globalConfig.js
+++ server/utils/globalConfig.js
@@ -952,6 +952,12 @@ if(mode === 'staging') {
 
 if(mode === 'preprod') {
 
+    //AWS SQS Email Configuration
+    config.aws_sqs_email_accessKeyId = "AKIAWIPBVOFR5CAQ735U";
+    config.aws_sqs_email_secretAccessKey = "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";
+    config.aws_sqs_email_region = "ap-south-1";
+    config.emailSQSQueueUrl = "https://sqs.ap-south-1.amazonaws.com/430506864995/ews-engine-mail";
+
     //Cognito
     config.cognito_region = 'ap-south-1';
     config.user_pool_id = 'ap-south-1_Ccmp0pMyI';
