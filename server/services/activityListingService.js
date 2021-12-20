1325e0840cdaab74fc5d9691f4c2b56c76665331
Akshay Singh <rakshaysingh888@gmail.com>
2021-12-20 14:50:14 +0530

    sqs added to global config

--- server/services/activityListingService.js
+++ server/services/activityListingService.js
@@ -4,7 +4,15 @@
 
 const { serializeError } = require("serialize-error");
 const logger = require("../logger/winstonLogger");
-const { Kafka } = require('kafkajs');
+
+const AWS = require('aws-sdk');
+AWS.config.update({
+	"accessKeyId": "AKIAWIPBVOFRSFSVJZMF",
+	"secretAccessKey": "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",
+	"region": "ap-south-1"
+});
+const sqs = new AWS.SQS();
+const uuidv4 = require('uuid/v4');
 
 function ActivityListingService(objCollection) {
 
@@ -4422,22 +4430,13 @@ function ActivityListingService(objCollection) {
 			// })
 			// producer.disconnect();
 
-
-			const AWS = require('aws-sdk');
-			AWS.config.update({
-				"accessKeyId": "AKIAWIPBVOFRSFSVJZMF",
-				"secretAccessKey": "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",
-				"region": "ap-south-1"
-			});
-			const sqs = new AWS.SQS();
-			const uuidv4 = require('uuid/v4');
 			sqs.sendMessage({
 				// DelaySeconds: 5,
 				MessageBody: JSON.stringify({
 					...request,
 					requestType: "summary_mom_child_orders"
 				}),
-				QueueUrl: "https://sqs.ap-south-1.amazonaws.com/430506864995/staging-child-orders-creation-v1.fifo",
+				QueueUrl: global.config.ChildOrdersSQSqueueUrl,
 				MessageGroupId: `mom-creation-queue-v1`,
 				MessageDeduplicationId: uuidv4(),
 				MessageAttributes: {
