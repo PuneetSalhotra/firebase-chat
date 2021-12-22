030e288180337d3aeefe1ea73c26e00aaf172099
shankar1525 <55125365+shankar1525@users.noreply.github.com>
2021-12-22 11:03:08 +0530

    on hold changes

--- server/Administrator/services/adminOpsService.js
+++ server/Administrator/services/adminOpsService.js
@@ -130,10 +130,11 @@ function AdminOpsService(objectCollection) {
             request.organization_flag_enable_timetracker || 0,
             request.organization_flag_timeline_access_mgmt || 0,
             request.flag_timeline_lead_mgmt || 0,
+            request.flag_dashboard_onhold || 0,
             request.asset_id,
             util.getCurrentUTCTime()
         );
-        const queryString = util.getQueryString('ds_p1_5_organization_list_update_flags', paramsArr);
+        const queryString = util.getQueryString('ds_p1_6_organization_list_update_flags', paramsArr);
 
         if (queryString !== '') {
             await db.executeQueryPromise(0, queryString, request)
@@ -968,11 +969,12 @@ function AdminOpsService(objectCollection) {
           request.organization_flag_enable_timetracker || 0,
           request.organization_flag_timeline_access_mgmt || 0,
           request.flag_lead_mgmt || 0,
+          request.flag_dashboard_onhold || 0,
           request.organization_type_id || 1,
           request.asset_id || 1,
           util.getCurrentUTCTime()
         );
-        const queryString = util.getQueryString('ds_p1_8_organization_list_insert', paramsArr);
+        const queryString = util.getQueryString('ds_p1_9_organization_list_insert', paramsArr);
 
         if (queryString !== '') {
             await db.executeQueryPromise(0, queryString, request)
