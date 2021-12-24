d931da0598fa625b7ca258583b4d683e7e5b64ab
shankar1525 <55125365+shankar1525@users.noreply.github.com>
2021-12-24 11:21:24 +0530

    activity_type changes

--- server/Administrator/services/adminOpsService.js
+++ server/Administrator/services/adminOpsService.js
@@ -147,7 +147,7 @@ function AdminOpsService(objectCollection) {
         }
         return [error, responseData];
     }
-
+    
     // Create Asset Bundle
     async function createAssetBundle(request, workforceID, organizationID, accountID) {
         // Performs multiple steps
@@ -988,7 +988,7 @@ function AdminOpsService(objectCollection) {
         }
         return [error, responseData];
     }
-
+    
     // Organization List History Insert
     async function organizationListHistoryInsert(request) {
         let responseData = [],
@@ -7643,13 +7643,12 @@ console.log('new ActivityId321',newActivity_id)
         return [error, responseData];
     }
 
-    async function createDefaultWidgets(request,tag_type_id){
+    async function createDefaultWidgets (request,tag_type_id){
         let responseData = [],
         error = true;
     const paramsArr = new Array(
         request.organization_id,
         request.widget_type_category_id || 3,
-        tag_type_id,
         request.workforce_id,
         0,
         50
@@ -7659,8 +7658,9 @@ console.log('new ActivityId321',newActivity_id)
 
     if (queryString !== '') {
         await db.executeQueryPromise(0, queryString, request)
-            .then((data1) => {
+            .then(async (data1) => {
                let widgetData = require('../../utils/defaultWidgetMappings.json');
+               let [actError,activity_type] = await adminListingService.workforceActivityTypeMappingSelectCategory({...request,activity_type_category_id:58})
             //    console.log(widgetData)
                for(let i=0;i<widgetData.length;i++){
                   let checkExistingWidgets = data1.findIndex((item)=>item.widget_type_id==widgetData[i].widget_type_id);
@@ -7696,6 +7696,8 @@ console.log('new ActivityId321',newActivity_id)
                     "widget_id": -1,
                     "widget_target_value": "",
                     "widget_type_category_id": 3,
+                    activity_type_category_id:58,
+                    activity_type_id:activity_type[0].activity_type_id
                   }
                   widgetDataToSend = {...widgetDataToSend,...widgetData[i]};
                   let requestToSend = {...request,...widgetDataToSend};
