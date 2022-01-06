7410b05ff9011d7eb7cd15ed60a8921251e8db31
Mangesh91 <mangeshrs91@gmail.com>
2022-01-06 14:11:27 +0530

    added one new parameter 'filter_organaziatioin_id' and also changes done sp names

--- server/analytics/services/analyticsService.js
+++ server/analytics/services/analyticsService.js
@@ -2318,6 +2318,7 @@ function AnalyticsService(objectCollection)
                         request.filter_field_entity_5 || '',
                         request.organization_onhold || 0,
                         request.widget_activity_id || 0,
+                        request.filter_organization_id || -1,
                         parseInt(request.page_start) || 0,
                         parseInt(request.page_limit) || 50
                     );
@@ -2329,7 +2330,7 @@ function AnalyticsService(objectCollection)
                    
                         for(let iteratorM = 0; iteratorM < counter; iteratorM++){
                              paramsArray.push(iteratorM);
-                            tempResult = await db.callDBProcedureR2(request, 'ds_v2_activity_search_list_select_widget_values', paramsArray, 1); 
+                            tempResult = await db.callDBProcedureR2(request, 'ds_v2_1_activity_search_list_select_widget_values', paramsArray, 1); 
                             paramsArray.pop();
                             responseArray.push(tempResult[0])
                         }
@@ -2349,7 +2350,7 @@ function AnalyticsService(objectCollection)
                     } else {
                         console.log(paramsArray);
                         paramsArray.push(0);
-                        tempResult = await db.callDBProcedureR2(request, 'ds_v2_activity_search_list_select_widget_values', paramsArray, 1); paramsArray.pop();
+                        tempResult = await db.callDBProcedureR2(request, 'ds_v2_1_activity_search_list_select_widget_values', paramsArray, 1); paramsArray.pop();
                         console.log(tempResult);
                      //   let widgetTypes = [23,24,48,49,63,66,37,38,65,61,67,53,54, 39, 40, 41, 42];
                      //   if(widgetTypes.includes(request.widget_type_id)){
@@ -2573,7 +2574,7 @@ function AnalyticsService(objectCollection)
                 responseJson.sequence_id = widgetFlags[iteratorM];
                 verticalResponseAdditonalMap.set(iteratorM, responseJson);
 
-                const queryString = util.getQueryString('ds_v1_9_activity_search_list_select_widget_values_oppty', paramsArray);
+                const queryString = util.getQueryString('ds_v2_1_activity_search_list_select_widget_values_oppty', paramsArray);
                 if (queryString !== '') {
 
                     await db.executeQueryPromise(1, queryString, request)
@@ -2716,7 +2717,7 @@ function AnalyticsService(objectCollection)
                 responseJson.sequence_id = widgetFlags[iteratorM];
                 verticalResponseAdditonalMap.set(iteratorM, responseJson);
 
-                const queryString = util.getQueryString('ds_v1_9_activity_search_list_select_widget_values_oppty', paramsArray);
+                const queryString = util.getQueryString('ds_v2_1_activity_search_list_select_widget_values_oppty', paramsArray);
                 if (queryString !== '') {
 
                     await db.executeQueryPromise(1, queryString, request)
@@ -2903,7 +2904,7 @@ function AnalyticsService(objectCollection)
 
                 verticalResponseAdditonalMap.set(iteratorM, responseJson);
 
-                const queryString = util.getQueryString('ds_v1_9_activity_search_list_select_widget_values_oppty', paramsArray);
+                const queryString = util.getQueryString('ds_v2_1_activity_search_list_select_widget_values_oppty', paramsArray);
                 if (queryString !== '') {
 
                     await db.executeQueryPromise(1, queryString, request)
@@ -3155,10 +3156,11 @@ function AnalyticsService(objectCollection)
                      request.filter_field_entity_4 || '',
                      request.filter_field_entity_5 || '',
                      request.organization_onhold || 0,
-                     request.widget_activity_id || 0
+                     request.widget_activity_id || 0,
+                     request.filter_organization_id || -1
                  );
             
-            let queryString = util.getQueryString('ds_v2_activity_search_list_select_widget_drilldown_search', paramsArray);
+            let queryString = util.getQueryString('ds_v2_1_activity_search_list_select_widget_drilldown_search', paramsArray);
                 if (queryString !== '') {
                     tempResult = await (db.executeQueryPromise(1, queryString, request));
                 }
@@ -3328,10 +3330,13 @@ function AnalyticsService(objectCollection)
                      request.filter_field_entity_2 || '',
                      request.filter_field_entity_3 || '',
                      request.filter_field_entity_4 || '',
-                     request.filter_field_entity_5 || ''
+                     request.filter_field_entity_5 || '',
+                     request.organization_onhold || 0,
+                     request.widget_activity_id || 0,
+                     request.filter_organization_id || -1
                     );
             
-            let queryString = util.getQueryString('ds_v1_9_activity_search_list_select_widget_drilldown_oppty', paramsArray);
+            let queryString = util.getQueryString('ds_v2_1_activity_search_list_select_widget_drilldown_oppty', paramsArray);
                 if (queryString !== '') {
                     tempResult = await (db.executeQueryPromise(1, queryString, request));
                 }
@@ -5075,6 +5080,7 @@ function AnalyticsService(objectCollection)
 
     //----------------------------------------------------------------------
     //Mangesh S
+    //analytics/management/widget/value/resource
     //Get resource details for specified vertical_tag_id
     this.getManagementWidgetValueResource = async (request) => {
 
