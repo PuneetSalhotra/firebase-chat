9293b7bc5e2a899f5436766fa5c9ab858e6c8d0e
shankar1525 <55125365+shankar1525@users.noreply.github.com>
2022-02-21 10:30:31 +0530

    tasi changes

--- server/TASI/controllers/tasiController.js
+++ server/TASI/controllers/tasiController.js
@@ -1098,6 +1098,16 @@ function TasiController(objCollection) {
             res.json(responseWrapper.getResponse(err, resData, -9999, req.body));
         }
     });
+
+    app.post('/' + global.config.version + '/tasi/entity/target/mapping/outlier/flag/update', async function (req, res) {
+        const [err, resData] = await tasiService.entityTargetMappingOutlierFlagUpdate(req.body);
+        if (!err) {
+            res.json(responseWrapper.getResponse({}, resData, 200, req.body));
+        } else {
+            console.log("/tasi/entity/target/mapping/outlier/flag/update | Error: ", err);
+            res.json(responseWrapper.getResponse(err, resData, -9999, req.body));
+        }
+    });
 }
 
 module.exports = TasiController;
