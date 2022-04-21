3a9a290bb805711d7059f7a3104a6f66d81420f1
Akshay Singh R <rakshaysingh888@gmail.com>
2022-04-21 17:11:05 +0530

    desker sqs access key changed

--- botConsumer.js
+++ botConsumer.js
@@ -86,8 +86,8 @@ redisClient.on('connect', async function (response) {
             const botService = new BotService(serviceObjectCollection);
 
             AWS.config.update({
-                "accessKeyId": "AKIAWIPBVOFRSFSVJZMF",
-                "secretAccessKey": "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",
+                "accessKeyId": "AKIAWIPBVOFR4QJ3TS6E",
+                "secretAccessKey": "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",
                 "region": "ap-south-1"
             });
 
